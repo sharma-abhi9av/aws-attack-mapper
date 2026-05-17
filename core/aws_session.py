962a5f68281ba1201f0d3c54c7c8f8839e7d043e
sharma-abhi9av <in.abhi.linked@gmail.com>
2026-05-17 21:35:50 +0530

    modified:   core/aws_session.py

--- core/aws_session.py
+++ core/aws_session.py
@@ -2,3 +2,19 @@ import sys
 import botocore.exceptions
 import boto3
 
+class AWSSession:
+    def __init__(self,profile_name = None, region_name = None, endpoint_url = None):
+        self._endpoint_url = endpoint_url
+        try:
+            self._session = boto3.Session(
+                profile_name =profile_name,
+                region_name=region_name,
+            )
+        except botocore.exceptions.ProfileNotFound as e:
+            print(f"AWS profile not found: {e}")
+            sys.exit(1)
+    def get_client(self,service):
+        kwargs ={}
+        if self._endpoint_url:
+            kwargs["endpoint_url"] = self._endpoint_url
+        return self._session.client(service, **kwargs)
