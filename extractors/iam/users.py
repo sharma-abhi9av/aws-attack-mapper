926f088f1c892258427fe7a0e42250ee303e5b56
sharma-abhi9av <in.abhi.linked@gmail.com>
2026-05-19 09:55:39 +0530

    modified:   extractors/iam/users.py

--- extractors/iam/users.py
+++ extractors/iam/users.py
@@ -1,6 +1,27 @@
+import boto3
 from core.base_extractor import BaseExtractor
 
 class UsersExtractor(BaseExtractor):
     SERVICE_NAME="iam"
     def extract(self):
-        return self._paginate("list_users", "Users")
+        users = self._paginate("list_users", "Users")
+        enriched = []
+        for user in users:
+            username = user["UserName"]
+            # Groups --> list_groups_for_user, key = Groups 
+            user["Groups"] = self._paginate("list_groups_for_user", "Groups", UserName=username)
+            # Attached policies --> list_attached_user_policies, key = AttachedPolicies
+            user["AttachedPolicies"] = self._paginate("list_attached_user_policies", "AttachedPolicies", UserName=username)
+            # Inline policy names  --> list_user_policies, key = PolicyNames 
+            policy_names = self._paginate("list_user_policies", "PolicyNames", UserName=username)
+            user["InlinePolicies"] = []
+            for policy_name in policy_names:
+                resp= self._safe_call("get_user_policy", UserName=username, PolicyName=policy_name)
+                if resp:
+                    user["InlinePolicies"].append({
+                        "PolicyName": policy_name
+                        "Document"  : resp["PolicyDocument"]
+                    })
+            enriched.append(user)
+        return enriched
+    
